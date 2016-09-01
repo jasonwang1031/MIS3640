961fa69b8e974df878bf110bb942ec1e17d0101f
jasonwang1031 <ywang17@babson.edu>
2016-09-01 16:11:55 -0400

    calc.py

--- calc.py
+++ calc.py
@@ -1,8 +1,19 @@
 print(46 + 37 +38)
 print(2016-1995)
 print(15/3)
-
-
 print(3*10)
-
 print(3**10)
+
+
+print('exercise 2')
+print('Question 1:')
+print(42*60+42)
+print('Question 2:')
+print(10/1.61)
+
+print('Question 3:')
+print(42.7/6.21)
+
+print(2562/6.21)
+
+print(6.21/0.712)
