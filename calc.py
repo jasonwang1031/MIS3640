25fe562a783109e5a96a971ca3b0303a36753019
jasonwang1031 <ywang17@babson.edu>
2016-09-07 17:08:54 -0400

    calc.py

--- calc.py
+++ calc.py
@@ -17,3 +17,29 @@ print(42.7/6.21)
 print(2562/6.21)
 
 print(6.21/0.712)
+
+print('Date:%2d-%02d' %(9, 7)) # homework after session 2; due on 9.7 night
+print('question 1')
+import math # import math module; find this code online    
+π = math.pi
+r = 5
+print('sophere = ',4/3*π*r**3)
+
+print('question 2')
+C=24.95*0.4*60+3+59*0.75
+print('The total cost is %.2f' %(C))
+
+print('question 3')
+easypace = 15/60 + 8 #compute the speed of easy pace in minute/mile
+tempo = 12/60 + 7 #compute the speed of tempo in minute/mile
+Time = easypace*(1+1) + tempo*3 #compute the total time spend on running
+print('The total minutes I spend on running is',Time)
+Seconds = 0.1 * 60
+Minute = 52 + 38 - 60
+Hour = 6 + 1
+print('The time I come back home is %2d : %2d : %02d'%(Hour,Minute,Seconds))
+print('question 4')
+I = ((89-82)/82)
+I = I * 100
+print('The average grade increaces by %.1f%%' %(I))
+
